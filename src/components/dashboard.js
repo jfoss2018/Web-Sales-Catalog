379f339b0bdb0d73bd7d492021b72297462f7405
James Foss <jamesfoss1@hotmail.com>
2019-02-17 15:27:28 -0500

    Updated Dashboard Layout

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -11,6 +11,40 @@ const Dashboard = () => {
           <li>Manage Contents</li>
           <li>Manage Visitors</li>
         </ul>
+        <div className="user-block">
+          <div className="user-controls">
+            <div className="user-create user-control-btn">
+            </div>
+            <div className="user-edit user-control-btn">
+            </div>
+            <div className="user-delete user-control-btn">
+            </div>
+          </div>
+          <div className="user-list-wrapper">
+            <table className="user-list">
+              <tr>
+                <th>Record</th>
+                <th>Name</th>
+                <th>Auth-Level</th>
+              </tr>
+              <tr>
+                <th>1</th>
+                <th>John Smith</th>
+                <th>0</th>
+              </tr>
+              <tr>
+                <th>2</th>
+                <th>John Doe</th>
+                <th>1</th>
+              </tr>
+              <tr>
+                <th>3</th>
+                <th>John Wayne</th>
+                <th>2</th>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
